c9948688348022598e8a97edbce294ff12a14177
aaronjoshua27 <73965654+aaronpangan@users.noreply.github.com>
2021-11-21 06:11:16 +0800

    Initialize App

---
+++ src/test/java/com/jpa/jparest/JpaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.jparest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
