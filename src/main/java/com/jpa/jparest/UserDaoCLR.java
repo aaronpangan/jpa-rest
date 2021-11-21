78fa632a608c5e0e7165d5927802561af485af3a
aaronjoshua27 <73965654+aaronpangan@users.noreply.github.com>
2021-11-22 06:36:04 +0800

    Initialize Data Repository

--- src/main/java/com/jpa/jparest/UserDaoCLR.java
+++ src/main/java/com/jpa/jparest/UserDaoCLR.java
@@ -1,5 +1,7 @@
 package com.jpa.jparest;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +14,21 @@ public class UserDaoCLR implements CommandLineRunner {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private UserRepository userRepo;
+
     private static final Logger log = LoggerFactory.getLogger(UserDaoCLR.class);
 
     @Override
     public void run(String... args) throws Exception {
         UserEntity user = new UserEntity("Aaa", "bbbb");
-        long Id = userDao.insert(user);
+
+         long Id = userDao.insert(user);
+            Optional<UserEntity> us = userRepo.findById(Id);
+           
+
+        System.out.println(us);
+        
         log.info("New User Created");
     }
 
